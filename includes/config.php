abe648e34fee4e36f6b340d392d9d700d1682941
Quentin Bonhoure <bonhoure.quentin@gmail.com>
2024-11-28 19:45:08 +0100

    Fix fichier manquant

--- includes/config.php
+++ includes/config.php
@@ -1,9 +1,9 @@
 <?php
 // Paramètres de connexion à la base de données
-define('DB_SERVER', 'mysql-tp-git-dev.mysql.database.azure.com');    // Serveur de la base de données
-define('DB_USERNAME', 'esgiuser');                                           // Nom d'utilisateur de la base de données
-define('DB_PASSWORD', 'P@ssw0rd123!');                                           // Mot de passe de la base de données
-define('DB_NAME', 'party_planner_db');                               // Nom de la base de données
+define('DB_SERVER', 'mysql-tp-git-dev.mysql.database.azure.com');// Serveur de la base de données
+define('DB_USERNAME', 'esgiuser');                                     // Nom d'utilisateur de la base de données
+define('DB_PASSWORD', 'P@ssw0rd123!');                                     // Mot de passe de la base de données
+define('DB_NAME', 'party_planner_db');                         // Nom de la base de données
 
 try {
     $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
